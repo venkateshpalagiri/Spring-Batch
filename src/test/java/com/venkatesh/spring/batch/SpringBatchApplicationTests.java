0d523a0c618565dd4dbaea50b9e93ad45960459e
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-10-16 22:25:12 -0400

    init setup

---
+++ src/test/java/com/venkatesh/spring/batch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venkatesh.spring.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
