25989fbcde7b5d9c0aaf1ecd6bfb7c1db2fe70e6
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-10-21 11:17:33 -0400

    Step & Job created

--- src/main/java/com/venkatesh/spring/batch/CsvBatchConfig.java
+++ src/main/java/com/venkatesh/spring/batch/CsvBatchConfig.java
@@ -1,12 +1,18 @@
 package com.venkatesh.spring.batch;
 
 import com.venkatesh.spring.batch.entity.Employees;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
@@ -15,8 +21,15 @@ import org.springframework.core.io.FileSystemResource;
 @EnableBatchProcessing
 public class CsvBatchConfig {
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
     //    create Reader
-    public FlatFileItemReader<Employees> flatFileItemReader(){
+    public FlatFileItemReader<Employees> employeesReader(){
         FlatFileItemReader<Employees> fileItemReader=new FlatFileItemReader<>();
         fileItemReader.setResource(new FileSystemResource("src/main/resource/employees.csv"));
         fileItemReader.setStrict(false);
@@ -49,9 +62,33 @@ public class CsvBatchConfig {
     }
 
     //    create Writer
+    @Bean
+    public RepositoryItemWriter<Employees> repositoryItemWriter(){
+        RepositoryItemWriter<Employees> employeesRepositoryItemWriter=new RepositoryItemWriter<>();
+        employeesRepositoryItemWriter.setRepository(employeeRepository);
+        employeesRepositoryItemWriter.setMethodName("save");
+
+        return employeesRepositoryItemWriter;
+
+    }
 
     //    create Step
+    @Bean
+    public Step step1(){
+        return stepBuilderFactory.get("step-1").<Employees,Employees>chunk(10)
+                .reader(employeesReader())
+                .processor(employeesProcessor())
+                .writer(repositoryItemWriter())
+                .build();
+    }
 
     //    create Job
+    @Bean
+    public Job job(){
+        return jobBuilderFactory.get("employees-job")
+                .flow(step1())
+                .end()
+                .build();
+    }
 }
 
