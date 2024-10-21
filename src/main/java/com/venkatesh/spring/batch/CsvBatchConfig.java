ebe674992a18ce89885bb29d9ab62a2a69db5e32
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-10-20 20:21:07 -0400

    create Reader (ItemReader)

---
+++ src/main/java/com/venkatesh/spring/batch/CsvBatchConfig.java
@@ -0,0 +1,50 @@
+package com.venkatesh.spring.batch;
+
+import com.venkatesh.spring.batch.entity.Employees;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+@EnableBatchProcessing
+public class CsvBatchConfig {
+
+    //    create Reader
+    public FlatFileItemReader<Employees> flatFileItemReader(){
+        FlatFileItemReader<Employees> fileItemReader=new FlatFileItemReader<>();
+        fileItemReader.setResource(new FileSystemResource("src/main/resource/employees.csv"));
+        fileItemReader.setStrict(false);
+        fileItemReader.setLineMapper(lineMapper());
+    }
+
+    private LineMapper<Employees> lineMapper() {
+        DefaultLineMapper<Employees> lineMapper=new DefaultLineMapper<>();
+
+        DelimitedLineTokenizer tokenizer=new DelimitedLineTokenizer();
+        tokenizer.setDelimiter(",");
+        tokenizer.setNames("csv_batch");
+        tokenizer.setNames("id","firstName","lastName","email","gender","contactNo","country","dob");
+
+        BeanWrapperFieldSetMapper<Employees> fieldSetMapper=new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Employees.class);
+
+        lineMapper.setLineTokenizer(tokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+        return lineMapper;
+
+    }
+
+    //    create Processor
+
+    //    create Writer
+
+    //    create Step
+
+    //    create Job
+}
+
